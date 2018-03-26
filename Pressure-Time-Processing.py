690b35918dd9fa16a104f30e7cc2113f72ab630e
Karan Davis <karandavis@u.boisestate.edu>
2018-03-26 11:14:55 -0600

    More changes
    Got the program working almost completely cleanly, the only major issues right now are that it doesn't handle data that's corrupted farther than the first few rows. If data is corrupted deep within the file, it stops the program altogether.

--- Pressure-Time-Processing.py
+++ Pressure-Time-Processing.py
@@ -1,5 +1,6 @@
 import glob
 import math
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
@@ -10,15 +11,25 @@ filenames = sorted(glob.glob('DATA-0*.CSV'))
 #Grabbing the line before start time because the loop I have
 #set up reads the next line
 search_string = ";Version"
-files_in_range = []
+#Declaring the arrays for time and pressure
+pressure_data = []
+time_data = []
+
+#Defining a function for the median absolute deviation in order to throw out 
+#values that are more than 10 stdev away
+def mad(a):
+    med = np.median(a)
+    mad = np.median(np.absolute(a - med))
+    return mad 
 
 for a in filenames:
     #scanning the file for the right line, then grabbing the numbers that are
     #on the next line
     with open(a, mode='r', encoding='iso-8859-1') as x:
-        reader = csv.reader(y.replace('\0', '') for y in x)
+        reader = csv.reader(y.replace('\0', ' ') for y in x) #replace empty values
+        next(reader, None)
         for row in reader:
-            if search_string in row:
+            if search_string in row: #Searches for start date/time so that it can be grabbed and used
                 line = x.readline()
                 tokens = str.split(line, ',')
                 start_time = tokens[2]
@@ -28,19 +39,42 @@ for a in filenames:
                 year = int(start_date_split[0])
                 month = int(start_date_split[1])
                 day = int(start_date_split[2])
-                print(year, month, day)
                 
-                if year == 2017 and month in range(7,9):
-                    files_in_range.append(a)
+                start_time_split = start_time.split(':') #splits the the time into its components
+                hour = int(start_time_split[0])
+                minute = int(start_time_split[1])
+                second = float(start_time_split[2])
+                
+                if year == 2017 and month in range(7,9): #only uses the data for graphing if it is in range
                     print(a)
-                    break
-                break
+                    df = pd.read_csv(a, encoding="ISO-8859-1", header=None, usecols=[0,1]) #Grabbing the col needed
+                    time_data = df.iloc[8:,:1] #From row 8 to the EOF for time data
+                    pressure_data = df.iloc[8:,1:2] #From row 8 to EOF for pressure data
+                    
+                    #Turning the strings from the dataframes into numbers
+                    #TODO: Build a try/catch to toss out corrupted data that can happen deep in file
+                    pressure_data = pressure_data.apply(pd.to_numeric) 
+                    time_data = time_data.apply(pd.to_numeric)
+                    
+                    #Gets rid of data that's more than 10 stdev away from MAD
+                    stdev = 1.4826 * mad(pressure_data)
+                    ind = (abs(pressure_data) > (10*stdev))
+                    
+                    masked_pressure = pressure_data[ind]
+                    
+                    #Create plots for each set of data
+                    plt.plot(time_data, masked_pressure, '.', ls='')
+                    plt.title("Pressure vs Time from " + str(a))
+                    plt.xlabel("Time (sec from " + str(start_time) + "on " + str(start_date) + ")")
+                    plt.ylabel("Pressure (Pa)")
+                    plt.show()
+                           
+            
         
+   
     
-#Using glob to go through this process with every CSV file. It will create a graph
-#of pressure v time for each given CSV
-for f in files_in_range:
-    #Creating the data array that will hold the first four rows
-    data = np.genfromtxt(f, comments = ';', delimiter = ',', filling_values = math.nan)
-    data = data[:,:4]
+    
+    
+    
+    
                     
